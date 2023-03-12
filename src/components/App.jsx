604c58edaf53cf20a9a50a359a4ddc961a2df2a4
Nazar Mosula <nmosula@gmail.com>
2023-03-12 12:17:20 +0200

    Changed Class to Func
    Add useState, useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,85 +1,76 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import UserForm from './UserForm';
 import UserList from './UserList';
 import Layout from './Layout';
 import Filter from './Filter';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
 
+  const defaultContacts = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
 
-  componentDidMount() {
+  function onLoadPage() {
     const savedContacts = localStorage.getItem('contacts');
 
     if (savedContacts !== null) {
       const parsedContacts = JSON.parse(savedContacts);
-      this.setState({ contacts: parsedContacts });
-      return;
+      return parsedContacts;
     }
-    this.setState({
-      contacts: [
-        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-      ],
-      
-    })
+
+    return defaultContacts;
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
 
-  addContact = newContact => {
-    if (this.state.contacts.find(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())) {
+  const [contacts, setContacts] = useState(onLoadPage);
+  const [filter, setFilter] = useState('');
+
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    setContacts(contacts);
+  }, [contacts])
+
+  const addContact = newContact => {
+    if (contacts.find(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())) {
       alert(`${newContact.name} is already in contacts`);
       return;
     }
-    this.setState(prevState => {
-      return {
-        contacts: [...prevState.contacts, newContact],
-      };
-    });
+
+    setContacts(prevContacts => [newContact, ...prevContacts]);
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-      };
-    });
+  const deleteContact = contactId => {
+    setContacts(prevContacts => prevContacts.filter(contact => contact.id !== contactId))
   };
 
-  filterContacts = event => {
-    this.setState({ filter: event.currentTarget.value });
+  const filterContacts = event => {
+    setFilter(event.currentTarget.value );
   };
 
-  getFilteredContacts = () => {
-    return this.state.contacts.filter(contact =>
-        contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+  const getFilteredContacts = () => {
+    return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase().trim())
     );
   };
 
 
-  render() {
-    return (
-      <Layout>
-        <UserForm onSave={this.addContact} />
-        <h2>Contacts</h2>
-        {this.state.contacts.length !== 0 ? (
-          <>
-            <Filter value={this.state.filter} onChange={this.filterContacts} />
-            <UserList items={this.getFilteredContacts()} onDelete={this.deleteContact} />
-          </>
-          ) : ( <h3>There are no contacts in your phonebook!</h3> )
-        }
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      <UserForm onSave={addContact} />
+      <h2>Contacts</h2>
+      {contacts.length !== 0 ? (
+        <>
+          <Filter value={filter} onChange={filterContacts} />
+          <UserList items={getFilteredContacts()} onDelete={deleteContact} />
+        </>
+        ) : ( <h3>There are no contacts in your phonebook!</h3> )
+      }
+    </Layout>
+  );
 }
+
+export default App;
